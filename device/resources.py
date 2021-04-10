2c83fa8f76445368573a0f69228e5a82cf7f738b
jeeten <swain.jeeten@gmail.com>
2021-04-10 18:33:53 +0530

    optimising code

--- device/resources.py
+++ device/resources.py
@@ -1,6 +1,7 @@
 from import_export import resources
 from .models import device, oem, model
 from django.core.exceptions import ValidationError
+from django.utils.safestring import mark_safe
 import re
 import datetime
 from django.utils.encoding import force_str
@@ -17,64 +18,62 @@ class DeviceResource(resources.ModelResource):
     # def export(self):
 
     def before_import_row(self, row, **kwargs):
-        name = str(row.get('oem'))
-        name.strip()
 
         Error = {}
-        if len(name) != 0:
+
+        name = row.get('oem')
+
+        if isinstance(name,str) and len(name.strip()) != 0:
+
             brand,_create = oem.objects.get_or_create(name=name.lower().strip())
             row['oem'] = brand.id
         else:
             Error.update({'OEM': ["Please enter valid Oem!."]})
             # raise ValidationError("OEM, Please enter valid Oem!.")
 
-        modelName = str(row.get('model'))
-        modelName.strip()
-        if len(modelName) != 0:
-            brandModelData = {"name": modelName.lower().strip(), "oem": name.lower().strip()}
-            # artist_id, created = Track.objects.get_or_create(artist=Artist(title=artist.title))
+        modelName = row.get('model')
+        # modelName = modelName.strip()
+        # print("model - {}".format(modelName))
+        if isinstance(modelName,str) and len(modelName.strip()) != 0:
             brand_model,_create = model.objects.get_or_create(name=modelName.lower().strip(),oem=brand)
             row['model'] = brand_model.id
         else:
             Error.update({'MODEL': ["Please enter valid Model!."]})
             # raise ValidationError("MODEL, Please enter valid Model!.")
 
-        dalivery = str(row.get('delivery')).strip()
-
-        if len(dalivery) == 0:
-            Error.update({'DELIVERY': ["Please enter valid Delivery!."]})
-        else:
+        dalivery = row.get('delivery')
+        # dalivery = str(dalivery or None)
+        # print(type(dalivery))
+        if isinstance(dalivery,str) and len(dalivery.strip()) != 0:
             if dalivery not in dict(device.delivary_type).values():
                 daliveryStr = ', '.join([str(x) for x in dict(device.delivary_type).values()])
                 Error.update({'DELIVERY': ["Please enter valid Delivery!,should be any of these {} ".format(daliveryStr)]})
+        else:
+            Error.update({'DELIVERY': ["Please enter valid Delivery!."]})
 
-        assignee = str(row.get('assignee'))
-        assignee = assignee.strip()
-        if len(assignee) == 0:
+
+        assignee = row.get('assignee')
+        # assignee = assignee.strip()
+        # assignee = str(assignee or None)
+        if assignee is None:
             row['assignee'] = None
 
-        imei = str(row.get('imei'))
-        imei = int(float(imei))
+        imei = row.get('imei')
 
-        if not re.match(r'^[0-9]{15}$',str(imei)):
-            Error.update({'IMEI': ["Please enter valid Imei!."]})
-            # raise ValidationError("IMEI, Please enter valid Imei!.")
-
-        # imei = str(imei).split('.')[0]
-        # print(imei)
-        # row['imei'] = imei
+        if isinstance(imei,str) and len(imei.strip()) != 0:
+            imei = int(float(imei))
+            if not re.match(r'^[0-9]{15}$',str(imei)):
+                Error.update({'IMEI': ["Please enter valid Imei!."]})
 
         assign_date = row.get('assigned_date')
-
-
-        if isinstance(assign_date, float):
+        if isinstance(assign_date, float) and assign_date is not None:
             datetime_date = xlrd.xldate_as_datetime(assign_date, 0)
             date_object = datetime_date.date()
             assign_date = date_object.strftime('%m/%d/%Y')
-        elif isinstance(assign_date, str):
+        elif isinstance(assign_date, str) and assign_date is not None and len(assign_date.strip()) != 0:
             assign_date = assign_date.strip()
 
-            if len(assign_date) != 0:
+            if assign_date:
                 date_format = '%m/%d/%Y'
                 try:
                     datetime.datetime.strptime(assign_date, date_format)
@@ -84,17 +83,18 @@ class DeviceResource(resources.ModelResource):
                     # raise ValidationError("Incorrect data format, should be MM/DD/YYYY")
         else:
             row['assigned_date'] = None
+
         return_date = ''
         return_date = row.get('return_date')
 
-        if isinstance(return_date, float):
+        if isinstance(return_date, float) and return_date is not None:
             datetime_date = xlrd.xldate_as_datetime(return_date, 0)
             date_object = datetime_date.date()
             return_date = date_object.strftime('%m/%d/%Y')
-        elif isinstance(return_date, str):
+        elif isinstance(return_date, str) and return_date is not None and len(assign_date.strip()) != 0:
             return_date = return_date.strip()
 
-            if len(return_date) != 0:
+            if return_date:
 
                 date_format = '%m/%d/%Y'
                 try:
